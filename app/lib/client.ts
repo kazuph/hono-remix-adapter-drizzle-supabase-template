1fb056a104022e7288527336a36254c5aa082150
Kazuhiro Homma <kazu.homma@gmail.com>
2024-12-07 23:16:55 +0900

    hono rpc clientで認証したい場合は、cookieを引き回さないといけない

--- app/lib/client.ts
+++ app/lib/client.ts
@@ -5,5 +5,5 @@ export function getApiClient(request: Request) {
   const url = new URL(request.url);
   const baseUrl = `${url.protocol}/${url.host}`;
 
-  return hc<AppType>(baseUrl);
+  return hc<AppType>(baseUrl, { headers: { Cookie: request.headers.get("Cookie") ?? "" } });
 }
